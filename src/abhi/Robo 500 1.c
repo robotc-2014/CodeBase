1c525e6987ac12f4cb585a92d30771668ce13dc4
robotc-abhi <abhi@hariharan.com>
2014-03-09 14:28:57 -0400

    Robo 500 1
    The Robo 500 1.

---
+++ src/abhi/Robo 500 1.c
@@ -0,0 +1,67 @@
+
+task main()
+{
+	motor[motorB]=50;
+	motor[motorC]=50;
+	wait1Msec(6500);
+
+	motor[motorB]=0;
+	motor[motorC]=50;
+	wait1Msec(1500);
+
+	motor[motorB]=50;
+	motor[motorC]=50;
+	wait1Msec(6500);
+
+	motor[motorB]=0;
+	motor[motorC]=50;
+	wait1Msec(1500);
+
+	motor[motorB]=50;
+	motor[motorC]=50;
+	wait1Msec(7090);
+
+	motor[motorB]=0;
+	motor[motorC]=50;
+	wait1Msec(1500);
+
+	motor[motorB]=50;
+	motor[motorC]=50;
+	wait1Msec(7090);
+
+	motor[motorB]=0;
+	motor[motorC]=50;
+	wait1Msec(1500);
+
+		motor[motorB]=50;
+	motor[motorC]=50;
+	wait1Msec(7090);
+
+	motor[motorB]=0;
+	motor[motorC]=50;
+	wait1Msec(1500);
+
+	motor[motorB]=50;
+	motor[motorC]=50;
+	wait1Msec(7090);
+
+	motor[motorB]=0;
+	motor[motorC]=50;
+	wait1Msec(1500);
+
+	motor[motorB]=50;
+	motor[motorC]=50;
+	wait1Msec(7090);
+
+	motor[motorB]=0;
+	motor[motorC]=50;
+	wait1Msec(1500);
+
+	motor[motorB]=50;
+	motor[motorC]=50;
+	wait1Msec(7090);
+
+
+
+
+}
