d4c33cfe9f257705dcd908cd1f28f709657b6f52
robotc-aaron <amaskarenas@gmail.com>
2014-03-16 14:27:22 -0400

    Created
    Uploading program for the first time.

---
+++ src/aaron/RoboSlalom.c
@@ -0,0 +1,92 @@
+
+task main()
+{
+
+
+	motor[motorB] = 50;
+	motor[motorC]= 50;
+	wait1Msec(1000);
+
+	motor[motorB] = -50;
+	motor[motorC]= 50;
+	wait1Msec(1000);
+
+	motor[motorB] = 50;
+	motor[motorC]= 50;
+	wait1Msec(3000);
+
+	motor[motorB] = 50;
+	motor[motorC]= -50;
+	wait1Msec(1000);
+
+	motor[motorB] = 50;
+	motor[motorC]= 50;
+	wait1Msec(3000);
+
+	motor[motorB] = 50;
+	motor[motorC]= -50;
+	wait1Msec(1000);
+
+	motor[motorB] = 50;
+	motor[motorC]= 50;
+	wait1Msec(3000);
+
+	motor[motorB] = -50;
+	motor[motorC]= 50;
+	wait1Msec(500);
+
+	motor[motorB] = 50;
+	motor[motorC]= 50;
+	wait1Msec(2500);
+
+	motor[motorB] = -50;
+	motor[motorC]= 50;
+	wait1Msec(500);
+
+
+
+	motor[motorB] = 50;
+	motor[motorC]= 50;
+	wait1Msec(2000);
+
+	motor[motorB] = -50;
+	motor[motorC]= 50;
+	wait1Msec(1000);
+
+	motor[motorB] = 50;
+	motor[motorC]= 50;
+	wait1Msec(5000);
+
+	motor[motorB] = 50;
+	motor[motorC]= -50;
+	wait1Msec(1000);
+
+	motor[motorB] = 50;
+	motor[motorC]= 50;
+	wait1Msec(3000);
+
+	motor[motorB] = 50;
+	motor[motorC]= -50;
+	wait1Msec(1000);
+
+	motor[motorB] = 50;
+	motor[motorC]= 50;
+	wait1Msec(4000);
+
+	motor[motorB] = -50;
+	motor[motorC]= 50;
+	wait1Msec(500);
+
+	motor[motorB] = 50;
+	motor[motorC]= 50;
+	wait1Msec(2500);
+
+	motor[motorB] = -50;
+	motor[motorC]= 50;
+	wait1Msec(500);
+
+	motor[motorB] = 50;
+	motor[motorC]= 50;
+	wait1Msec(2500);
+
+}
