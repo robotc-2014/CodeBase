7e52fe8fce875a93575689a12ad8b0f5e4256dd8
robotc-neeharpolavarapu <npolavarapu17@gmail.com>
2014-03-16 15:49:04 -0400

    Update line_painter.c

--- src/neeharpolavarapu/line_painter.c
+++ src/neeharpolavarapu/line_painter.c
@@ -1,1 +1,44 @@
-//Hint: use the sine wave
+void drawLine(int distance);
+void penDown();
+void penUp();
+
+task main()
+{
+  int testDistance = 300;
+  drawLine(testDistance);
+  // turnLeft();
+  // drawLine(8);
+  // turnRight();
+  // drawLine(20);
+  // turnRight();
+  // drawLine(8);
+  // turnLeft();
+  // drawLine(5);
+}
+
+void drawLine(int distance) {
+  penDown();
+  //reset encoders
+  nMotorEnocder[motorB] = 0;
+  nMotorEnocder[motorC] = 0;
+  motor[motorB] = 50;
+  motor[motorC] = 50;
+  while (nMotorEncoder[motorB] < distance) {
+  }
+  penUp();
+  motor[motorB] = 0;
+  motor[motorC] = 0;
+}
+
+void penDown() {
+  motor[motorA] = 50;
+  wait1Msec(100);
+  motor[motorA] = 0;
+}
+
+void penUp() {
+  motor[motorA] = -50;
+  wait1Msec(100);
+  motor[motorA] = 0;
+}
+//End of Line Painter Challenge
